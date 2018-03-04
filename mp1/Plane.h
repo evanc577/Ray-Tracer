948f0fdf3211b278e028b276e6a1f71f065a31cf
Evan Chang <evanc577@gmail.com>
2018-03-04 05:37:54 -0600

    plane support (kinda)

---
+++ mp1/Plane.h
@@ -0,0 +1,16 @@
+#ifndef PLANE_H
+#define PLANE_H
+
+#include "Hittable.h"
+
+class Plane : public Hittable {
+    public:
+        Plane();
+        Plane(vec3 p, vec3 n);
+        virtual bool hit(const Ray &r, float t_min, float t_max,
+                hit_record &rec) const;
+        vec3 point;
+        vec3 normal;
+};
+
+#endif
