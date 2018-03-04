da54c13b0de543525b987987ca60778ec6fadf2c
Evan Chang <evanc577@gmail.com>
2018-03-04 05:37:29 -0600

    support for multiple hittables

---
+++ mp1/HittableList.h
@@ -0,0 +1,33 @@
+#ifndef HITTABLE_LIST_H
+#define HITTABLE_LIST_H
+
+#include <vector>
+#include "Hittable.h"
+
+class HittableList : public Hittable {
+    public:
+        HittableList() {}
+        HittableList(std::vector<Hittable *> l) {
+            list_ = l;
+        }
+
+        virtual bool hit(const Ray &r, float t_min, float t_max,
+                hit_record &rec) const {
+            hit_record temp;
+            bool hit = false;
+            float closest = t_max;
+            for (unsigned long i = 0; i < list_.size(); i++) {
+                if (list_[i]->hit(r,t_min,closest, temp)) {
+                    hit = true;
+                    closest = temp.t;
+                    rec = temp;
+                }
+            }
+            return hit;
+        }
+
+        std::vector<Hittable *> list_;
+};
+
+
+#endif
