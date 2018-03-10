a983ec5f57a0c53f0007f2c43b10b0a39994621f
Evan Chang <evanc577@gmail.com>
2018-03-09 22:56:49 -0600

    added PointLight.cpp

---
+++ mp1/PointLight.cpp
@@ -0,0 +1,20 @@
+#include "PointLight.h"
+
+PointLight::PointLight() : Light() { point = vec3(0, 0, 0); }
+
+bool PointLight::AtPoint(const vec3 &p, std::vector<vec3> &ca,
+                         std::vector<vec3> &cd, std::vector<vec3> &cs,
+                         std::vector<vec3> &d) {
+  ca.clear();
+  cd.clear();
+  cs.clear();
+  d.clear();
+
+  float distance = (p - point).length();
+
+  ca.emplace_back(0, 0, 0);
+  cd.push_back(id / (distance * distance));
+  cs.push_back(is / (distance * distance));
+  d.push_back(normalize(point - p));
+  return true;
+}
