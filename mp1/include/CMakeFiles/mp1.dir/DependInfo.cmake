e98fc550c2f9cdf805108cb1e2d446494c3a52bc
Evan Chang <evanc577@gmail.com>
2018-03-21 01:23:45 -0500

    moved to cmake

---
+++ mp1/include/CMakeFiles/mp1.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/evan/cs/Ray-Tracer/mp1/src/AmbientLight.cpp" "/home/evan/cs/Ray-Tracer/mp1/include/CMakeFiles/mp1.dir/src/AmbientLight.cpp.o"
+  "/home/evan/cs/Ray-Tracer/mp1/src/CheckerPlane.cpp" "/home/evan/cs/Ray-Tracer/mp1/include/CMakeFiles/mp1.dir/src/CheckerPlane.cpp.o"
+  "/home/evan/cs/Ray-Tracer/mp1/src/DirectionalLight.cpp" "/home/evan/cs/Ray-Tracer/mp1/include/CMakeFiles/mp1.dir/src/DirectionalLight.cpp.o"
+  "/home/evan/cs/Ray-Tracer/mp1/src/HittableList.cpp" "/home/evan/cs/Ray-Tracer/mp1/include/CMakeFiles/mp1.dir/src/HittableList.cpp.o"
+  "/home/evan/cs/Ray-Tracer/mp1/src/Image.cpp" "/home/evan/cs/Ray-Tracer/mp1/include/CMakeFiles/mp1.dir/src/Image.cpp.o"
+  "/home/evan/cs/Ray-Tracer/mp1/src/LightList.cpp" "/home/evan/cs/Ray-Tracer/mp1/include/CMakeFiles/mp1.dir/src/LightList.cpp.o"
+  "/home/evan/cs/Ray-Tracer/mp1/src/Plane.cpp" "/home/evan/cs/Ray-Tracer/mp1/include/CMakeFiles/mp1.dir/src/Plane.cpp.o"
+  "/home/evan/cs/Ray-Tracer/mp1/src/PointLight.cpp" "/home/evan/cs/Ray-Tracer/mp1/include/CMakeFiles/mp1.dir/src/PointLight.cpp.o"
+  "/home/evan/cs/Ray-Tracer/mp1/src/RayTracer.cpp" "/home/evan/cs/Ray-Tracer/mp1/include/CMakeFiles/mp1.dir/src/RayTracer.cpp.o"
+  "/home/evan/cs/Ray-Tracer/mp1/src/Sphere.cpp" "/home/evan/cs/Ray-Tracer/mp1/include/CMakeFiles/mp1.dir/src/Sphere.cpp.o"
+  "/home/evan/cs/Ray-Tracer/mp1/src/Triangle.cpp" "/home/evan/cs/Ray-Tracer/mp1/include/CMakeFiles/mp1.dir/src/Triangle.cpp.o"
+  "/home/evan/cs/Ray-Tracer/mp1/src/main.cpp" "/home/evan/cs/Ray-Tracer/mp1/include/CMakeFiles/mp1.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ mp1/include/CMakeFiles/mp1.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mp1.dir/src/AmbientLight.cpp.o"
+  "CMakeFiles/mp1.dir/src/CheckerPlane.cpp.o"
+  "CMakeFiles/mp1.dir/src/DirectionalLight.cpp.o"
+  "CMakeFiles/mp1.dir/src/HittableList.cpp.o"
+  "CMakeFiles/mp1.dir/src/Image.cpp.o"
+  "CMakeFiles/mp1.dir/src/LightList.cpp.o"
+  "CMakeFiles/mp1.dir/src/Plane.cpp.o"
+  "CMakeFiles/mp1.dir/src/PointLight.cpp.o"
+  "CMakeFiles/mp1.dir/src/RayTracer.cpp.o"
+  "CMakeFiles/mp1.dir/src/Sphere.cpp.o"
+  "CMakeFiles/mp1.dir/src/Triangle.cpp.o"
+  "CMakeFiles/mp1.dir/src/main.cpp.o"
+  "mp1.pdb"
+  "mp1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mp1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
